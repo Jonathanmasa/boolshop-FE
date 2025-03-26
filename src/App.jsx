74223af68daa7d484c5a7d4866f8262b6d0457c8
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-03-26 09:36:38 +0100

    feat: integrate WishlistProvider and CartProvider into App.jsx for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // Import functions from React
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import context
-import GlobalContext from "./contexts/ProductContext";
+import ProductProvider from "./contexts/ProductContext";
+import { WishlistProvider } from './WishlistContext';
+import { CartProvider } from './CartContext';
 
 // Import layouts
 import DefaultLayout from "./layouts/DefaultLayout";
+
+
+
 // Import pages
 import HomePage from "./pages/HomePage";
 import ProductCard from "./components/ProductCard";
@@ -22,21 +27,23 @@ function App() {
   // RENDER
   return (
     <ProductProvider>
-
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index path="/" element={<HomePage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            {/* route 404 not found */}
-            <Route path="/footer/about_us" element={<AboutUs />} />
-            <Route path="/footer/contacts" element={<Contacts />} />
-            <Route path="/footer/privacy" element={<Privacy />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
+      <WishlistProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<DefaultLayout />}>
+                <Route index path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductDetailPage />} />
+                {/* route 404 not found */}
+                <Route path="/footer/about_us" element={<AboutUs />} />
+                <Route path="/footer/contacts" element={<Contacts />} />
+                <Route path="/footer/privacy" element={<Privacy />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      </WishlistProvider>
     </ProductProvider>
   );
 
