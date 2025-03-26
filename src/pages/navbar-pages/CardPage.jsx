89ea7dec9c000cf195b34120f567d9e4e0451bed
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-26 17:38:47 +0100

    create navbar-changes

---
+++ src/pages/navbar-pages/CardPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useProductContext } from '../contexts/ProductContext';
+import ProductGrid from '../components/ProductGrid';
+import { useLocation } from 'react-router-dom';
+
+
+const CardPage = () => {
+    const { fetchProducts, products, loading, error } = useProductContext();
+    const [searchTerm, setSearchTerm] = useState('');
+    useEffect(() => {
+        console.log("🔎 Nuova ricerca con query:", query);
+        if (query) {
+            // Aggiorna il termine di ricerca
+            setSearchTerm(query);
+            // Chiamata per ottenere i prodotti
+            fetchProducts('/api/products/category/card', {
+                query,
+                sort
+            });
+
+
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Risultati della ricerca per "{searchTerm}"</h1>
+            {loading ? (
+                <p>Caricamento...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
+                <ProductGrid products={products} />
+            )}
+        </div>
+    );
+};
+
+export default CardPage;
