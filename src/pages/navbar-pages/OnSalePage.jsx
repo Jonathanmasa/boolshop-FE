b4107cf97882edcb0dbf3d2b9d19d04d09058d5d
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-27 10:47:20 +0100

    fix pages structure, feat CartContext and Cart logic

--- src/pages/navbar-pages/OnSalePage.jsx
+++ src/pages/navbar-pages/OnSalePage.jsx
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useProductContext } from '../../contexts/ProductContext';;
 import ProductGrid from '../../components/ProductGrid';
-import { useLocation } from 'react-router-dom';
+import { useCartContext } from '../../contexts/CartContext';
 
 
 const OnSalePage = () => {
     const { fetchProducts, products, loading, error } = useProductContext();
-    const [searchTerm, setSearchTerm] = useState('');
+    const { addToCart, removeFromCart } = useCartContext();
     useEffect(() => {
 
         // Chiamata per ottenere i prodotti
         fetchProducts('/api/products/on-sale',);
-
     }, []);
 
     return (
         <div>
-            <h1>Risultati della ricerca per "{searchTerm}"</h1>
+            <h1>Risultati Promozioni</h1>
             {loading ? (
                 <p>Caricamento...</p>
             ) : error ? (
                 <p>{error}</p>
             ) : (
-                <ProductGrid products={products} />
+                <ProductGrid products={products}
+                            addToCart={addToCart}
+                            removeFromCart={removeFromCart}
+                />
             )}
         </div>
     );
