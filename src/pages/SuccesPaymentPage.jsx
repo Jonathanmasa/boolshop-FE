b848a9d920ffaf4907a50a24f0758c8c84db997c
Luca Belardinelli <belardinelliluca@gmail.com>
2025-04-02 12:25:01 +0200

    style: update layout of SuccessPaymentPage by changing to a centered container for improved presentation

--- src/pages/SuccesPaymentPage.jsx
+++ src/pages/SuccesPaymentPage.jsx
@@ -5,8 +5,8 @@ export default function ThankYouPage() {
     return (
 
         <div className="success-banner">
-            <div className='titlepage'>
-                <h2>Pagamento completato</h2>
+            <div className="container text-center my-5">
+                <h2 >Pagamento completato</h2>
                 <p>Grazie per il tuo ordine! Riceverai presto una conferma via email.</p>
             </div>
         </div >
